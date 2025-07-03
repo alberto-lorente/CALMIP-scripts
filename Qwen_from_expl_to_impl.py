cfe4905e1c4a22021837bd1ece09a10ac5bec601
Alberto Lorente Galé <albertolorente97@gmail.com>
2025-07-03 10:47:33 +0800

    Update Qwen_from_expl_to_impl.py

--- Qwen_from_expl_to_impl.py
+++ Qwen_from_expl_to_impl.py
@@ -259,11 +259,11 @@ OUTPUT AND FORMAT: your output should be just the label."""
     cols_to_remove = ["clean_post", "post", "class", "implicit_class", "extra_implicit_class", "target", "implied_statement", "split", "time", "formatted_prompt", "label", "__index_level_0__"]
 
     for split in hf_time_1:
-        # if split != "test":
-        #     hf_time_1[split] = hf_time_1[split].remove_columns(cols_to_remove)
-        #     hf_time_2[split] = hf_time_2[split].remove_columns(cols_to_remove)
-        hf_time_1[split] = hf_time_1[split].remove_columns(cols_to_remove)
-        hf_time_2[split] = hf_time_2[split].remove_columns(cols_to_remove)
+        if split != "test":
+            hf_time_1[split] = hf_time_1[split].remove_columns(cols_to_remove)
+            hf_time_2[split] = hf_time_2[split].remove_columns(cols_to_remove)
+        # hf_time_1[split] = hf_time_1[split].remove_columns(cols_to_remove)
+        # hf_time_2[split] = hf_time_2[split].remove_columns(cols_to_remove)
 
 
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
@@ -519,7 +519,7 @@ OUTPUT AND FORMAT: your output should be just the label."""
         print()
         print("List labels")
         print(labels_test)
-        
+
     if local_rank == 0:
         print("_________________________________")
         print("Saving the model and Tokenizer")
